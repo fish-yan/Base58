534ced6eaa99b2779644aeede1c7f399e332d6a5
yan <15316488+fish-yan@users.noreply.github.com>
2025-03-11 14:20:59 +0800

    init

--- Package.swift
+++ Package.swift
@@ -11,11 +11,16 @@ let package = Package(
             name: "Base58",
             targets: ["Base58"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.5.1"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Base58"),
+            name: "Base58",
+            dependencies: ["CryptoSwift"]
+        ),
         .testTarget(
             name: "Base58Tests",
             dependencies: ["Base58"]
--- Tests/Base58Tests/Base58Tests.swift
+++ Tests/Base58Tests/Base58Tests.swift
@@ -1,6 +1,9 @@
 import Testing
 @testable import Base58
 
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+@Test func base58Test() {
+    let publick = "0x928e3d9740e13980bfcbcd0f7c9571f82df0c283de5cbf0125f6295896e80e16"
+    let address = "KsyWm1TQa9K2wbNNttoPVNG3JbUL9G3mhRrepfjYQ4NVngpyhygN1Xu9NfqMxV5utchN8XohHM1MsFAwz8kFC91h6M"
+    let result = publick.base58EncodedString
+    #expect(result == address)
 }
